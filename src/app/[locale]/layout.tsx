1a01476e2d782a9ee3ce2fbc5406b127f4bae501
Gerardo De La Cruz <netoxas3107@hotmail.com>
2024-01-06 11:47:35 +0100

    Apple touch icon ussue that didn't let app to display GD icon when installed on mobile phones fixed

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,21 +1,21 @@
-import { NextIntlClientProvider } from 'next-intl';
-import { notFound } from 'next/navigation';
+import { NextIntlClientProvider } from "next-intl";
+import { notFound } from "next/navigation";
 
 //Fonts
-import { roboto, fira } from '@font/font';
+import { roboto, fira } from "@font/font";
 
 //Styles
-import '@styles/normalize.css';
-import '@styles/globals.css';
+import "@styles/normalize.css";
+import "@styles/globals.css";
 
 //Components
-import Providers from '@components/generalUse/providers';
-import Header from '@components/specificUse/Header';
-import Footer from '@components/specificUse/Footer';
+import Providers from "@components/generalUse/providers";
+import Header from "@components/specificUse/Header";
+import Footer from "@components/specificUse/Footer";
 
 //Types
 //imported
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
 //local
 type Props = {
   children: React.ReactNode;
@@ -23,13 +23,15 @@ type Props = {
 };
 
 //Metadata
+//Title and description is not here because it it added manually to the page, so that it is dynamic depending on the language
 export const metadata: Metadata = {
-  title: 'Gerardo De La Cruz',
-  description: "Gerardo De La Cruz's portfolio",
+  icons: {
+    apple: [{ url: "/favicon/apple-touch-icon.png", sizes: "180x180" }],
+  },
 };
 
 export function generateStaticParams() {
-  return [{ locale: 'en' }, { locale: 'es' }, { locale: 'de' }];
+  return [{ locale: "en" }, { locale: "es" }, { locale: "de" }];
 }
 
 export default async function RootLayout({
@@ -46,46 +48,46 @@ export default async function RootLayout({
 
   return (
     //We specify data-theme and style to match the attributes in the server and in the client and avoid mismatching errors
-    <html lang={locale} data-theme='dark' style={{ colorScheme: 'dark' }}>
+    <html lang={locale} data-theme="dark" style={{ colorScheme: "dark" }}>
       <Providers>
         {/* makes browser color match website */}
-        <meta name='theme-color' content='#ffffff' />
+        <meta name="theme-color" content="#ffffff" />
 
         {/* allows search engines to find this page */}
-        <meta name='robots' content='index, follow' />
+        <meta name="robots" content="index, follow" />
 
         {/* Favicon */}
+        {/* <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="favicon/apple-touch-icon.png"
+        /> */}
         <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='favicon/apple-touch-icon.png'
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favicon/favicon-32x32.png"
         />
         <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='favicon/favicon-32x32.png'
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="favicon/favicon-16x16.png"
         />
+        <link rel="manifest" href="favicon/site.webmanifest" />
         <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='favicon/favicon-16x16.png'
-        />
-        <link rel='manifest' href='favicon/site.webmanifest' />
-        <link
-          rel='mask-icon'
-          href='favicon/safari-pinned-tab.svg'
-          color='#5bbad5'
+          rel="mask-icon"
+          href="favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
         />
         {/* background color of tile application in windows */}
-        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name="msapplication-TileColor" content="#da532c" />
         {/* makes browser color match website */}
-        <meta name='theme-color' content='#ffffff' />
+        <meta name="theme-color" content="#ffffff" />
         {/* Esta última línea debe estar así para evitar scroll horizontal por animaciones */}
         <meta
-          name='viewport'
-          content='width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+          name="viewport"
+          content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
 
         <body className={`${roboto.variable} ${fira.variable}`}>
