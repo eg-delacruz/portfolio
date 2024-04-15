3b5e175a281946a9adbbcc5251c281bb9a4a1935
Gerardo De La Cruz <netoxas3107@hotmail.com>
2024-04-15 14:33:24 +0200

    feat: add click icon to certificate cards when hover. Improve text hierarchy and padding

--- src/components/specificUse/CertificateCard/index.tsx
+++ src/components/specificUse/CertificateCard/index.tsx
@@ -1,9 +1,14 @@
-'use client';
+"use client";
+import Image from "next/image";
 
-import { useTranslations } from 'next-intl';
+import { useTranslations } from "next-intl";
 
 //Styles
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
+
+//Assets
+import white_click_icon from "@assets/icons/click_white.svg";
+import black_click_icon from "@assets/icons/click_black.svg";
 
 //Types
 //Local
@@ -29,23 +34,38 @@ const Index = ({
 
   return (
     <a
-      className={`${styles.certificate} ${purple_bg ? styles.purple_bg : ''} ${
-        blue_bg ? styles.blue_bg : ''
+      className={`${styles.certificate} ${purple_bg ? styles.purple_bg : ""} ${
+        blue_bg ? styles.blue_bg : ""
       }`}
       href={url}
-      target='_blank'
+      target="_blank"
     >
-      <h4>{title}</h4>
-      <p>
-        <strong>{t(`Page.certificates.issued_by`)}</strong>
-        {': '}
-        {issued_by}
-      </p>
-      <p>
-        <strong>{t(`Page.certificates.date`)}</strong>
-        {': '}
-        {issue_date}
-      </p>
+      <div className={styles.info}>
+        <h4 className={styles.title}>{title}</h4>
+        <p className="small_paragraph">
+          <strong>{t(`Page.certificates.issued_by`)}</strong>
+          {": "}
+          {issued_by}
+        </p>
+        <p className="small_paragraph">
+          <strong>{t(`Page.certificates.date`)}</strong>
+          {": "}
+          {issue_date}
+        </p>
+      </div>
+
+      <Image
+        className={`${styles.click_icon} data-hide-on-theme-light`}
+        alt="click icon"
+        src={white_click_icon}
+        width={20}
+      />
+      <Image
+        className={`${styles.click_icon} data-hide-on-theme-dark`}
+        alt="click icon"
+        src={black_click_icon}
+        width={20}
+      />
     </a>
   );
 };
