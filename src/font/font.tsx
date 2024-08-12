8cd13c4529fb1630cfb29bb619ca194b552f6d32
Gerardo De La Cruz <netoxas3107@hotmail.com>
2024-08-13 00:05:35 +0200

    fix: use Roboto font instead of Open Sans, since now Roboto is working again

--- src/font/font.tsx
+++ src/font/font.tsx
@@ -1,14 +1,13 @@
-import { Open_Sans, Fira_Sans } from 'next/font/google';
+import { Roboto, Fira_Sans } from "next/font/google";
 
-//TODO: in the future, chech if the Roboto font is working again
-export const roboto = Open_Sans({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-  variable: '--roboto',
+export const roboto = Roboto({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--roboto",
 });
 
 export const fira = Fira_Sans({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-  variable: '--fira',
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--fira",
 });
